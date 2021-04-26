2864325b3567a88903ac1343859b8f17cd70de32
yhnjeong <yhnjeong@gmail.com>
2021-04-26 23:53:29 +0900

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,6 @@
 <?php
+
+/* php에서 mYsql 접속, SELECT, INSERT, UPDATE, DELETE*/
 function db_get_pdo(){
     $host = 'localhost';
     $port = '3306';
@@ -6,8 +8,8 @@ function db_get_pdo(){
     $charset = 'utf8';
     $username = 'php3min';
     $db_pw = "1234";
-    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
-    $pdo = new PDO($dsn, $username, $db_pw);
+    $dsn = "mysql: host=$host;port=$port;dbname=$dbname;charset=$charset";
+    $pdo = new PDO($dsn, $username, $db_pw);   // PDO는 PHP 내장 객체(PHP Database Object)
     return $pdo;
 }
 
@@ -26,10 +28,70 @@ function db_select($query, $param=array()){
     }
 }
 
-/*순차배열, 연관배열 이해하고, 쿼리 안에 파라미터 전달하는 것 다시 보자.*/
+function db_insert($query, $param = array()){
+    $pdo = db_get_pdo();
+    try {
+        $st = $pdo->prepare($query);
+        $result = $st->execute($param);
+        $last_id = $pdo->lastInsertId();
+        $pdo = null;
+        if ($result) {
+            return $last_id;
+        } else {
+            return false;
+        }
+    } catch(PDOException $ex){
+        return false;
+    } finally {
+        $pdo = null;
+    }
+}
 
+
+function db_update_delete($query, $param = array()) {
+    $pdo = db_get_pdo();
+    try {
+        $st = $pdo->prepare($query);
+        $result = $st->execute($param);
+        $pdo = null;
+        return $result;
+    } catch (PDOException $ex) {
+        return false;
+    } finally {
+        $pdo = null;
+    }
+}
+
+/* 쿼리 안에 파라미터 전달하는 2가지(순차배열, 연관배열) 방법.*/
+
+/* SELECT */
 var_dump(db_select("select * from tbl_person"));
 echo "<br />";
-var_dump(db_select("select * from tbl_person where person_id=?", array(1)));
+var_dump(db_select("select * from tbl_person where person_id=?", array(1))); //파라미터 배열은 순차배열로 전달
 echo "<br />";
-var_dump(db_select("select * from tbl_person where person_id = :person_id", array('person_id' => 1)));
+var_dump(db_select("select * from tbl_person where person_id = :person_id", array('person_id' => 1))); // 파라미터 배열은 연관배열로 전달
+echo "<br />";
+
+/* INSERT */
+var_dump(
+    db_insert("insert into tbl_person (name, age) values (?,?)", array("php", 25))
+);
+echo "<br />";
+var_dump(
+    db_insert("insert into tbl_person (name, age) values (:name, :age)", array("name"=>"php7", "age" =>5)
+    )
+);
+echo "<br />";
+
+/* UPDATE, DELETE */
+var_dump(
+    db_update_delete(
+        "update tbl_person set age = :age where person_id = :person_id", array("age"=>4, "person_id"=>1)
+    )
+    );
+echo "<br />";
+var_dump(
+    db_update_delete("delete from tbl_person where person_id=?", array(3))
+);
+echo "<br />";  
+?>
